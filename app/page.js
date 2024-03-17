83b5737e5b980cb0a1d056b77689ad224fa1be4a
JW148 <47108195+JW148@users.noreply.github.com>
2024-03-17 20:15:27 +0000

    Metadata

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import BackgroundReveal from "./ui/background";
 import Header from "./ui/header/header";
-
 import Section1 from "./sections/section1";
 import Section2 from "./sections/section2";
 import Section3 from "./sections/section3";
 import Section4 from "./sections/section4";
 import Section5 from "./sections/section5";
+
+export const metadata = {
+  title: "Rudi",
+  description: "Family run cafe since 1992",
+};
 
 export default function Home() {
   return (
